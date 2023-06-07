02f4ba9d6bbe4be4836431519f3ff4f98c3a8e6e
Tran Minh Thien <nhoktmt2002@gmail.com>
2023-06-07 23:14:22 +0700

    lab 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer/Footer";
+
 import Contact from "./pages/Contact/Contact";
 import { Home } from "./pages/Home/Home";
 import Detail from "./pages/Detail/Detail";
 import Navigation from "./components/Nav/Navigation/Navigation";
 import About from "./pages/About/About";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
